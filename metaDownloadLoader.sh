51b2698e70bd2f77e70c9da93ed0e3a91965d839
Radu Mihail Stroie <108636961+RaduStroie25@users.noreply.github.com>
2025-12-18 19:30:56 +0100

    Update metaDownloadLoader.sh
    bruh

--- metaDownloadLoader.sh
+++ metaDownloadLoader.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd ~/Desktop
-~/bin/cull -O https://raw.githubusercontent.com/RaduStroie25/downloadAPITest/refs/heads/main/LargerFile.txt
+curl -O https://raw.githubusercontent.com/RaduStroie25/downloadAPITest/refs/heads/main/LargerFile.txt
 exit
